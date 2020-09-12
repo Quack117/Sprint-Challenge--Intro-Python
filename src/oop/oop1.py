2233acb4fb7482b608e0e7c682a8d1dbb1c4252d
Ben Quackenbush <quackenbush117@gmail.com>
2020-09-11 19:23:48 -0600

    oop1 is complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,5 +18,23 @@
 #
 # Put a comment noting which class is the base class
 
-class Vehicle:
+class Vehicle: #base class, none
+    pass
+
+class FlightVehicle(Vehicle): #base class Vehicle
+    pass
+
+class Starship(FlightVehicle): #base class FlightVehicle
+    pass
+
+class Airplane(FlightVehicle): #base class FlightVehicle
+    pass
+
+class GroundVehicle(Vehicle): #base class Vehicle
+    pass
+
+class Car(GroundVehicle): #base class GroundVehicle
+    pass
+
+class Motorcycle(GroundVehicle): #base class GroundVehicle
     pass
